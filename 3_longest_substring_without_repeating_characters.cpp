e4e7178bcd648bcfefff3450f19dc3039c9b5435
zisheng <103584547+zisheng7777@users.noreply.github.com>
2025-01-16 14:36:29 +0800

    Update 3_longest_substring_without_repeating_characters.cpp

--- 3_longest_substring_without_repeating_characters.cpp
+++ 3_longest_substring_without_repeating_characters.cpp
@@ -3,13 +3,13 @@ public:
     int lengthOfLongestSubstring(string s) {
         int table[128] = {0};
         int longest_length;
-        int max;
+        int max_length;
         for (int right=0, left=0; right<s.size(); right++){
             left = max[table[s[right]], left];
-            max = right - left + 1;
+            max_length = right - left + 1;
             table[s[right]] = right + 1;
         }
-        return max;
+        return max_length;
     }
 };
 
